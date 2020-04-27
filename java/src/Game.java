131cb6011134aec27d64cf3055ef36d310d34708
lamak <lamak.qaizar@careem.com>
2020-04-28 00:42:11 +0500

    removed comment code smell for tile is already played

--- java/src/Game.java
+++ java/src/Game.java
@@ -13,12 +13,15 @@ public class Game {
         else if (repeatedMove(symbol)) {
             throw new Exception("Invalid next player");
         }
-        //if not first move but play on an already played tile
-        else if (_board.TileAt(x, y).Symbol != SPACE) {
+        else if (tileIsAlreadyPlayed(x, y)) {
             throw new Exception("Invalid position");
         }
 
         updateGameState(symbol, x, y);
+    }
+
+    private boolean tileIsAlreadyPlayed(int x, int y) {
+        return _board.TileAt(x, y).Symbol != SPACE;
     }
 
     private boolean repeatedMove(char symbol) {
@@ -40,9 +43,9 @@ public class Game {
 
     public char Winner() {
         //if the positions in first row are taken
-        if (_board.TileAt(0, 0).Symbol != SPACE &&
-                _board.TileAt(0, 1).Symbol != SPACE &&
-                _board.TileAt(0, 2).Symbol != SPACE) {
+        if (tileIsAlreadyPlayed(0, 0) &&
+                tileIsAlreadyPlayed(0, 1) &&
+                tileIsAlreadyPlayed(0, 2)) {
             //if first row is full with same symbol
             if (_board.TileAt(0, 0).Symbol ==
                     _board.TileAt(0, 1).Symbol &&
@@ -52,9 +55,9 @@ public class Game {
         }
 
         //if the positions in first row are taken
-        if (_board.TileAt(1, 0).Symbol != SPACE &&
-                _board.TileAt(1, 1).Symbol != SPACE &&
-                _board.TileAt(1, 2).Symbol != SPACE) {
+        if (tileIsAlreadyPlayed(1, 0) &&
+                tileIsAlreadyPlayed(1, 1) &&
+                tileIsAlreadyPlayed(1, 2)) {
             //if middle row is full with same symbol
             if (_board.TileAt(1, 0).Symbol ==
                     _board.TileAt(1, 1).Symbol &&
@@ -65,9 +68,9 @@ public class Game {
         }
 
         //if the positions in first row are taken
-        if (_board.TileAt(2, 0).Symbol != SPACE &&
-                _board.TileAt(2, 1).Symbol != SPACE &&
-                _board.TileAt(2, 2).Symbol != SPACE) {
+        if (tileIsAlreadyPlayed(2, 0) &&
+                tileIsAlreadyPlayed(2, 1) &&
+                tileIsAlreadyPlayed(2, 2)) {
             //if middle row is full with same symbol
             if (_board.TileAt(2, 0).Symbol ==
                     _board.TileAt(2, 1).Symbol &&
