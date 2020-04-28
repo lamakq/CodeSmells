4b3d37100c36ca10f54de6a5d6a741ce324b0528
lamak <lamak.qaizar@careem.com>
2020-04-28 14:33:51 +0500

    migrated tests to use new Play method

--- java/src/Game_Should.java
+++ java/src/Game_Should.java
@@ -13,42 +13,42 @@ public class Game_Should {
 
     @Test(expected=Exception.class)
     public void NotAllowPlayerOToPlayFirst() throws Exception {
-        game.Play('O', 0, 0);
+        game.Play(Symbol.O, 0, 0);
     }
 
     @Test(expected=Exception.class)
     public void NotAllowPlayerXToPlayTwiceInARow() throws Exception
     {
-        game.Play('X', 0, 0);
+        game.Play(Symbol.X, 0, 0);
 
-        game.Play('X', 1, 0);
+        game.Play(Symbol.X, 1, 0);
     }
 
     @Test(expected=Exception.class)
     public void NotAllowPlayerToPlayInLastPlayedPosition() throws Exception
     {
-        game.Play('X', 0, 0);
+        game.Play(Symbol.X, 0, 0);
 
-        game.Play('O', 0, 0);
+        game.Play(Symbol.O, 0, 0);
     }
 
     @Test(expected=Exception.class)
     public void NotAllowPlayerToPlayInAnyPlayedPosition() throws Exception
     {
-        game.Play('X', 0, 0);
-        game.Play('O', 1, 0);
+        game.Play(Symbol.X, 0, 0);
+        game.Play(Symbol.O, 1, 0);
 
-        game.Play('X', 0, 0);
+        game.Play(Symbol.X, 0, 0);
     }
 
     @Test
     public void DeclarePlayerXAsAWinnerIfThreeInTopRow() throws Exception
     {
-        game.Play('X', 0, 0);
-        game.Play('O', 1, 0);
-        game.Play('X', 0, 1);
-        game.Play('O', 1, 1);
-        game.Play('X', 0, 2);
+        game.Play(Symbol.X, 0, 0);
+        game.Play(Symbol.O, 1, 0);
+        game.Play(Symbol.X, 0, 1);
+        game.Play(Symbol.O, 1, 1);
+        game.Play(Symbol.X, 0, 2);
 
         Symbol winner = game.Winner();
 
@@ -58,12 +58,12 @@ public class Game_Should {
     @Test
     public void DeclarePlayerOAsAWinnerIfThreeInTopRow() throws Exception
     {
-        game.Play('X', 2, 2);
-        game.Play('O', 0, 0);
-        game.Play('X', 1, 0);
-        game.Play('O', 0, 1);
-        game.Play('X', 1, 1);
-        game.Play('O', 0, 2);
+        game.Play(Symbol.X, 2, 2);
+        game.Play(Symbol.O, 0, 0);
+        game.Play(Symbol.X, 1, 0);
+        game.Play(Symbol.O, 0, 1);
+        game.Play(Symbol.X, 1, 1);
+        game.Play(Symbol.O, 0, 2);
 
         Symbol winner = game.Winner();
 
@@ -73,11 +73,11 @@ public class Game_Should {
     @Test
     public void DeclarePlayerXAsAWinnerIfThreeInMiddleRow() throws Exception
     {
-        game.Play('X', 1, 0);
-        game.Play('O', 0, 0);
-        game.Play('X', 1, 1);
-        game.Play('O', 0, 1);
-        game.Play('X', 1, 2);
+        game.Play(Symbol.X, 1, 0);
+        game.Play(Symbol.O, 0, 0);
+        game.Play(Symbol.X, 1, 1);
+        game.Play(Symbol.O, 0, 1);
+        game.Play(Symbol.X, 1, 2);
 
         Symbol winner = game.Winner();
 
@@ -87,12 +87,12 @@ public class Game_Should {
     @Test
     public void DeclarePlayerOAsAWinnerIfThreeInMiddleRow() throws Exception
     {
-        game.Play('X', 0, 0);
-        game.Play('O', 1, 0);
-        game.Play('X', 2, 0);
-        game.Play('O', 1, 1);
-        game.Play('X', 2, 1);
-        game.Play('O', 1, 2);
+        game.Play(Symbol.X, 0, 0);
+        game.Play(Symbol.O, 1, 0);
+        game.Play(Symbol.X, 2, 0);
+        game.Play(Symbol.O, 1, 1);
+        game.Play(Symbol.X, 2, 1);
+        game.Play(Symbol.O, 1, 2);
 
         Symbol winner = game.Winner();
 
@@ -102,11 +102,11 @@ public class Game_Should {
     @Test
     public void DeclarePlayerXAsAWinnerIfThreeInBottomRow() throws Exception
     {
-        game.Play('X', 2, 0);
-        game.Play('O', 0, 0);
-        game.Play('X', 2, 1);
-        game.Play('O', 0, 1);
-        game.Play('X', 2, 2);
+        game.Play(Symbol.X, 2, 0);
+        game.Play(Symbol.O, 0, 0);
+        game.Play(Symbol.X, 2, 1);
+        game.Play(Symbol.O, 0, 1);
+        game.Play(Symbol.X, 2, 2);
 
         Symbol winner = game.Winner();
 
@@ -116,12 +116,12 @@ public class Game_Should {
     @Test
     public void DeclarePlayerOAsAWinnerIfThreeInBottomRow() throws Exception
     {
-        game.Play('X', 0, 0);
-        game.Play('O', 2, 0);
-        game.Play('X', 1, 0);
-        game.Play('O', 2, 1);
-        game.Play('X', 1, 1);
-        game.Play('O', 2, 2);
+        game.Play(Symbol.X, 0, 0);
+        game.Play(Symbol.O, 2, 0);
+        game.Play(Symbol.X, 1, 0);
+        game.Play(Symbol.O, 2, 1);
+        game.Play(Symbol.X, 1, 1);
+        game.Play(Symbol.O, 2, 2);
 
         Symbol winner = game.Winner();
 
